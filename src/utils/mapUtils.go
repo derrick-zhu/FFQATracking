7c464457be08735c4ec5638790624bd018b14ccb
Derrick ZHU <derrick.zhu@farfetch.com>
2018-08-28 00:38:27 +0800

    [add] db file; [add] UT for src/utils

--- src/utils/mapUtils.go
+++ src/utils/mapUtils.go
@@ -7,12 +7,31 @@ import (
 
 // Struct2Map struct mapping to map
 func Struct2Map(obj interface{}) map[string]interface{} {
+
 	t := reflect.TypeOf(obj)
 	v := reflect.ValueOf(obj)
 
 	var data = make(map[string]interface{})
+
 	for i := 0; i < t.NumField(); i++ {
-		data[strings.ToLower(t.Field(i).Name)] = v.Field(i).Interface()
+
+		if IsEmpty(v.Field(i)) == false {
+
+			data[strings.ToLower(t.Field(i).Name)] = v.Field(i).Interface()
+		}
 	}
+
 	return data
+}
+
+// IsEmpty if any object is empty or not
+func IsEmpty(a interface{}) bool {
+
+	v := reflect.ValueOf(a)
+	if v.Kind() == reflect.Ptr {
+
+		v = v.Elem()
+	}
+
+	return v.Interface() == reflect.Zero(v.Type()).Interface()
 }
