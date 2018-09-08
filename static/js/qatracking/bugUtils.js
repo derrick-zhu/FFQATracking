755843eb1c73cc4172f8d4177d886ab3994b7b5b
Zhu Delun <delun.zhu@gmail.com>
2018-09-08 15:16:27 +0800

    [wip] new issue page

---
+++ static/js/qatracking/bugUtils.js
@@ -0,0 +1,7 @@
+
+function submitNewBug() {
+    var title = document.getElementById("title").value;
+    var description = document.getElementById("description").value;
+
+    alert("title: " + title + " description:" + description);
+}
