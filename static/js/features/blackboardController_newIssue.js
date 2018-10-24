56c3e63a792b5b8d031314a0fd5de7bf2595a74d
Derrick ZHU <derrick.zhu@farfetch.com>
2018-10-24 17:31:37 +0800

    remove the issueNew.* page

--- static/js/features/blackboardController_newIssue.js
+++ static/js/features/blackboardController_newIssue.js
@@ -13,3 +13,16 @@ $(function () {
         );
     });
 });
+
+function newIssueCheckInputContent() {
+    if (checkInputLength('issueTitle', 1, 1024) == false) {
+        alert('invalid title');
+        return false;
+    }
+
+    if (checkInputLength('issueContent', 1, 3096) == false) {
+        alert('invalid description');
+        return false;
+    }
+}
+
